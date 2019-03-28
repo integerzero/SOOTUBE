f9668f228fd4e9e7af80da13b716445b8e440187
integerzero <steadyprogrammer@gmail.com>
2019-03-28 22:44:17 +0900

    Learn middleware

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const  express = require('express');
-const app = express()
+import express from "express";
+const app = express();
 
 const PORT = 4000;
-function handleLinstening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleLinstening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('hello from home');
+
+const handleProfile = (req, res) => res.send("You are on profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("I'm middleware");
+    next();
 }
+
+app.get("/", betweenHome, handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(4000, handleLinstening);
