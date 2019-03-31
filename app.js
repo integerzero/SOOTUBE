ba31cec196bd51b27201dbf02c0fbabe2965416d
integerzero <steadyprogrammer@gmail.com>
2019-03-31 15:43:28 +0900

    seperate Router

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 // import하는 쪽이 default가 아니기 때문에 이렇게 import해야함.
-import {userRouter} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
-
-const handleHome = (req, res) => res.send('hello from home');
-
-const handleProfile = (req, res) => res.send("You are on profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,12 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(logger("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-// use는 userRouter안에 있는 것들을 사용하겠다.
-app.use("/user", userRouter);
+// use는 Router안에 있는 것들을 사용하겠다.
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 // import하는 곳에 app object를 주겠다
 export default app;
