7fefff680a6fa5f2affd0ab748b361b5c5fa9a83
integerzero <steadyprogrammer@gmail.com>
2019-03-31 14:36:32 +0900

    Routing

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ import logger from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+// import하는 쪽이 default가 아니기 때문에 이렇게 import해야함.
+import {userRouter} from "./router";
 const app = express();
-
-const PORT = 4000;
-
-const handleLinstening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send('hello from home');
 
 const handleProfile = (req, res) => res.send("You are on profile");
 
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
@@ -24,4 +21,8 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(4000, handleLinstening);
+// use는 userRouter안에 있는 것들을 사용하겠다.
+app.use("/user", userRouter);
+
+// import하는 곳에 app object를 주겠다
+export default app;
