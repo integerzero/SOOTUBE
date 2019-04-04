9a37b4c18403286cc33c7cfd2c5190bb339cdab4
integerzero <steadyprogrammer@gmail.com>
2019-04-04 19:39:41 +0900

    create search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,16 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home"});
-export const search = (req, res) => res.render("search", { pageTitle: "Search"});
+export const search = (req, res) => {
+    // ES6때 방법은 아래와 같다.
+    // const searchingBy = req.query.term;
+
+    // 최신 버젼
+    const {
+        query: { term : searchingBy }
+    } = req;
+    console.log(searchingBy);
+
+    res.render("search", { pageTitle: "Search", searchingBy: searchingBy});
+}
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail",  { pageTitle: "Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video"});
