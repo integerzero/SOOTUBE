843ab2e2a5e3b66bb21dc4281a1102f0f73d2fe1
integerzero <steadyprogrammer@gmail.com>
2019-04-06 22:20:26 +0900

    Upload and creating a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,16 @@
-import {videos} from "../db";
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    res.render("home", { pageTitle: "Home", videos});
+export const home = async (req, res) => {
+    try {
+        // Search All videos in Database.
+        const videos = await Video.find({});
+        res.render("home", { pageTitle: "Home", videos});
+    } catch (error) {
+        console.log(error);
+        res.render("home", { pageTitle: "Home", videos: []});
+    }
+    
 }
 
 export const search = (req, res) => {
@@ -19,13 +27,19 @@ export const search = (req, res) => {
 }
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload"});
 
-export const postUpload = (req, res) => {
-    const {
-        body: { file, title, description }
+export const postUpload = async (req, res) => {
+    const { 
+        body: { title, description }, 
+        file: { path }
     } = req;
-    // TODO: Upload and save video
-
-    res.redirect(routes.videoDetail(324696));
+    
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title: title,
+        description: description
+    });
+    console.log(newVideo);
+    res.redirect(routes.videoDetail(newVideo.id));
 }
 
 export const videoDetail = (req, res) => res.render("videoDetail",  { pageTitle: "Video Detail"});
