11621f153cd633542f6660a869729442ada19aa3
integerzero <steadyprogrammer@gmail.com>
2019-04-07 23:04:19 +0900

    Editing a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,7 +10,6 @@ export const home = async (req, res) => {
         console.log(error);
         res.render("home", { pageTitle: "Home", videos: []});
     }
-    
 }
 
 export const search = (req, res) => {
@@ -42,6 +41,47 @@ export const postUpload = async (req, res) => {
     res.redirect(routes.videoDetail(newVideo.id));
 }
 
-export const videoDetail = (req, res) => res.render("videoDetail",  { pageTitle: "Video Detail"});
-export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video"});
+export const videoDetail = async (req, res) => {
+    const {
+        params: {id}
+    } = req;
+
+    try {
+        const video = await Video.findById(id);
+        res.render("videoDetail",  { pageTitle: "Video Detail", video:video});
+        //res.render("videoDetail",  { pageTitle: "Video Detail"});
+    } catch(error) {
+        console.log(error);
+        res.render(routes.home);
+    }
+};
+
+export const getEditVideo = async (req, res) => {
+    const {
+        params: {id}
+    } = req;
+    try {
+        const video = await Video.findById(id);
+        res.render("editVideo", {pageTitle: `Edit ${video.title}`, video});
+    } catch(error) {
+        res.redirect(routes.home);
+    }
+    res.render("editVideo", { pageTitle: "Edit Video"});
+};
+
+export const postEditVideo = async (req, res) => {
+    const {
+        params: {id},
+        body: {title, description}
+    } = req;
+    try {
+        // pug name이랑 model의 이름을 맞춰 주면 아래와 같이 코딩가능.
+        // await Video.findOneAndUpdate({id}, {title, description});
+        await Video.findOneAndUpdate({id: id}, {title: title, description: description});
+        res.redirect(routes.videoDetail(id));
+    } catch(error) {
+        res.redirect(routes.home);
+    }
+};
+
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video"});
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,18 +3,27 @@ import routes from "../routes";
 import { 
     upload, 
     videoDetail, 
-    editVideo, 
+    getEditVideo, 
     deleteVideo, 
-    postUpload
+    postUpload,
+    postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middleware";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, upload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
