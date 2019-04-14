02f1d1ca1288c2db4a0104d6a9908beff019f61c
integerzero <steadyprogrammer@gmail.com>
2019-04-14 18:43:06 +0900

    Create Local authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const join = (req, res) => {
     res.render("join", { pageTitle: "Join"});
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
     const {
         body: {name, email, password, password2}
     } = req;
@@ -13,7 +14,16 @@ export const postJoin = (req, res) => {
         res.status(400);
         res.render("join", { pageTitle: "Join"});
     } else {
-        // TODO: Register User
+        try {
+            const user = await User({
+                name,
+                email
+            });
+            await User.register(user, password);
+        } catch(error) {
+            console.log(error);
+        }
+        
         // TODO: Log user in
         res.redirect(routes.home);
     }
